839a2de56f5dfac86f72d05904197bd8015e488f
JSGodX <nehmegabriel@outlook.com>
2022-10-28 16:18:05 +0200

    new modifications
    make sure to read instructions.txt

--- js/drag.js
+++ js/drag.js
@@ -1,6 +1,8 @@
 // function that drags elements from toolbar to workspace
 
 function drag(element,location){
+    // the object is starting to get draged
+    // activates on mouse hold
     element.addEventListener('dragstart', e=>{
         e.dataTransfer.setData('text/plain',e.target.id);
         e.dataTransfer.setDragImage(w_a_d,w_a_d.width/2,w_a_d.height/2)// css
@@ -8,6 +10,8 @@ function drag(element,location){
         w_a_d.style.display="block";// css 
         location.style.border="5px dashed grey";// css
     });
+    // the user stoped dragging the object
+    // activates on mouse remove
     element.addEventListener('dragend',e=>{
         e.target.style.opacity =1;// css needed
         location.style.border="5px solid black";// css needed
@@ -15,11 +19,17 @@ function drag(element,location){
 }
 
 function location_drag(location){
+    // works like hover
+    // activates when hovering the dropzone with draggable
     location.addEventListener('dragenter',e=>{
     });
+    // works like hover:leave
+    // activates when hover leaving the dropzone with draggable
     location.addEventListener('dragleave',e=>{
         // css needed 
     });
+    // works when object has successfully been dragged 
+    // activates on mouse remove
     location.addEventListener('drop',e=>{
         e.preventDefault();
         const tool_id =e.dataTransfer.getData('text/plain');
@@ -28,6 +38,8 @@ function location_drag(location){
         e.target.appendChild(elem.clone())
         
     });
+    // similar to fiering dragenter sith set interval of 50ms
+    // activates when hovering the dropzone with draggable
     location.addEventListener('dragover',e=>{
         e.preventDefault();
     });
--- js/main.js
+++ js/main.js
@@ -1,59 +1,19 @@
-const draggable = document.querySelector("#test");
-const dropzone = document.querySelector("#workspace");
+/////////////////////////////////////////////////////////////////////////
+// drag and drop 
 
+const dropzone = document.querySelector("#workspace"); //#
 //* css needed
-const w_a_d = document.createElement("img")
-w_a_d.src = "images/what_a_drag.jpeg"
+const w_a_d = document.createElement("img");//#
+w_a_d.src = "images/what_a_drag.jpeg";
 //*
 
-draggable.cpt = 0
-draggable.clone = clone
+let tools = document.querySelectorAll(".tools");
 
-/////////////////////////////////////////////////////////////////////////
-// drag and drop -test
-
-// the object is starting to get draged
-// activates on mouse hold
-draggable.addEventListener('dragstart', e=>{
-    e.dataTransfer.setData('text/plain',e.target.id);
-    e.dataTransfer.setDragImage(w_a_d,w_a_d.width/2,w_a_d.height/2)// css
-    e.target.style.opacity =0.3;// css 
-    w_a_d.style.display="block";// css 
-    dropzone.style.border="5px dashed grey";// css
-});
-
-// the user stoped dragging the object
-// activates on mouse remove
-draggable.addEventListener('dragend',e=>{
-    e.target.style.opacity =1;// css needed
-    dropzone.style.border="5px solid black";// css needed
-});
-
-// works like hover
-// activates when hovering the dropzone with draggable
-dropzone.addEventListener('dragenter',e=>{
-    // css needed
-});
-
-// works like hover:leave
-// activates when hover leaving the dropzone with draggable
-dropzone.addEventListener('dragleave',e=>{
-    // css needed 
-});
-
-// works when object has successfully been dragged 
-// activates on mouse remove
-dropzone.addEventListener('drop',e=>{
-    e.preventDefault();
-    const draggable_id =e.dataTransfer.getData('text/plain');
-    e.target.appendChild(document.getElementById(draggable_id).clone())
-    // css needed
-});
-
-// similar to fiering dragenter sith set interval of 50ms
-// activates when hovering the dropzone with draggable
-dropzone.addEventListener('dragover',e=>{
-    e.preventDefault();
-});
+for(let tool of tools){
+	tool.cpt = 0;
+    tool.clone = clone;
+    drag(tool,dropzone);
+}
+location_drag(dropzone);
 
 /////////////////////////////////////////////////////////////////////////
