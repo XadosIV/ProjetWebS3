5c1540028979d075e2e1fc12942076653fe70601
JSGodX <nehmegabriel@outlook.com>
2022-10-30 18:31:49 +0100

    clone name
    modified clone name

--- js/clone_prim.js
+++ js/clone_prim.js
@@ -3,7 +3,7 @@ function clone_prime(elem){
     for(let er of tools){
         if(er.id==elem.id){
             er.idNbr++;
-            return create(er.item,dropzone,er.text,"tools",er.id+"_"+er.idNbr,er.draggable)
+            return create(er.item,dropzone,er.text,"cloned",er.id+"_"+er.idNbr,er.draggable)
         }
     }
 }
