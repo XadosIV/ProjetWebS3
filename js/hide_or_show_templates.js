81b508705f20c6c6ffa796e591e94e8a3160e838
JSGodX <nehmegabriel@outlook.com>
2022-11-13 18:18:42 +0100

    things

--- js/hide_or_show_templates.js
+++ js/hide_or_show_templates.js
@@ -1,12 +1,15 @@
 // function that hide or show templates
-const iframe = document.querySelector("#iframe")
+const iframe = document.querySelector("#iframe");//#
+const icon = document.querySelector("#temp_menu");//#
 var toggler = true;
 function on_click_template(element){
     if(toggler){
-        iframe.classList.remove("invisible");
+        iframe.style.display="flex";
+        icon.classList.add("invisible");
         toggler=false;
     }else{
-        iframe.classList.add("invisible");
+        iframe.style.display="none";
+        icon.classList.remove("invisible");
         toggler=true;
     }
 }
---
+++ js/horizantal_scroll.js
@@ -0,0 +1,4 @@
+// function that is used to scroll the templates
+function horizantal_scroll(){
+    
+}
