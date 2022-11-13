5303898478916a02954c5803a45d8df7d8111304
JSGodX <nehmegabriel@outlook.com>
2022-11-13 13:56:46 +0100

    modif
    the tab buttons change sizes automaatically,

--- js/user_create_page.js
+++ js/user_create_page.js
@@ -5,15 +5,31 @@ var pages = []
 function switch_pages(nb){
     var args = nb.split("_")
     nb = args[args.length-1]
+    // gab a modifer
+    const tab_width=tabs.offsetWidth;
+    var old_but_width = document.querySelector("#tab_button_0").offsetWidth;
+    var nbr_of_butt=Math.floor(tab_width/old_but_width)-1
+
     for (var i in pages){
         var button = document.querySelector("#tab_button_"+i)
         if (i == nb){
             pages[i].classList.remove("invisible")
             current_dropzone = pages[i]
             button.style.backgroundColor="white"
+            console.log(tabs.offsetWidth)
         }else{
             pages[i].classList.add("invisible")
             button.style.backgroundColor="buttonface"
+        }
+        // gab a modifer
+        // adjust the size of the buttons
+        b=parseInt(i);
+        if (b>=nbr_of_butt){
+            nbr_of_butt++;
+            for(var j=0; j<pages.length;j++){
+                var but = document.querySelector("#tab_button_"+j)
+                but.offsetWidth=tab_width/nbr_of_butt;
+            }
         }
     }
 }
