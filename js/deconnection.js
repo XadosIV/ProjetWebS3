1446d70dde69b7ef6575c00b32c4ebe7eec8ad06
MagicsssS <100727916+MagicsssS@users.noreply.github.com>
2022-11-28 08:40:23 +0100

    Hop profile affiché

--- js/deconnection.js
+++ js/deconnection.js
@@ -1,5 +1,5 @@
 function deconnection() {
-    var profileName = sessionStorage.removeItem('name');
-    var profileEmail = sessionStorage.removeItem('email');
+    sessionStorage.removeItem('name');
+    sessionStorage.removeItem('email');
     window.location.href = "index.html";
 }
