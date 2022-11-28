6d3250be95197fd0712c8ce8804b632057998ccf
MagicsssS <100727916+MagicsssS@users.noreply.github.com>
2022-11-28 10:31:15 +0100

    Sign_in/up.php ENCORE MIEUX

--- php/sign_in.php
+++ php/sign_in.php
@@ -7,24 +7,21 @@ $_POST = json_decode(file_get_contents("php://input"),true);
     if (isset($_POST['name2']) and isset($_POST['password2'])){ 
         $res=get_user($_POST['name2'], $_POST['password2']);
         if ($res != null){
-            $connected = True;  
             $connectedAs = array(
                 "name" => $res['name'], 
                 "email" => $res['email'],
                 "id" => $res['id']
             );   
+
+            echo (json_encode(array(
+                "leave" => True,
+                "profile" => $connectedAs
+            )));
+            exit;
         } else {
             echo "Name, email or password incorrect";
         }
     }		
-
-    if ($connected){
-        echo (json_encode(array(
-            "leave" => True,
-            "profile" => $connectedAs
-        )));
-        exit;
-    }  
 
 include("disconnectSQL.php");
 ?>
--- php/sign_up.php
+++ php/sign_up.php
@@ -9,28 +9,22 @@ $_POST = json_decode(file_get_contents("php://input"),true);
 if (isset($_POST['name1']) and isset($_POST['email']) and isset($_POST['password'])){ 
     $res=check_user($_POST['name1'], $_POST['email']);
     if ($res != null) {
-        //echo("<br><br><i><strong><span style='color:red'>*</span> Name already taken.</strong></i>");
         echo "Name or email already taken.";
-        $alreadyUsed = True;
-    }
-    
-    if (!$alreadyUsed){
+    } else {
         insert_user($_POST['name1'],$_POST['email'],$_POST['password']);         
-        $connected = True;        
+      
         $connectedAs = array(
             "name" => $_POST['name1'], 
             "email" => $_POST['email'],
-            "id" => $res['id']
-        );   
-    }        
+            "id" => get_user($_POST['name1'], $_POST['password'])['id']
+        ); 
 
-    if ($connected){
         echo (json_encode(array(
             "leave" => True,
             "profile" => $connectedAs
         )));
         exit;
-    }
+    }     
 }
 
 include("disconnectSQL.php");
