68d117410d61fd5c4e7cdb23d1550fa4f3043f5e
MagicsssS <100727916+MagicsssS@users.noreply.github.com>
2022-11-28 10:01:44 +0100

    Oubli

--- js/deconnection.js
+++ js/deconnection.js
@@ -1,5 +1,6 @@
 function deconnection() {
     sessionStorage.removeItem('name');
     sessionStorage.removeItem('email');
+    sessionStorage.removeItem('id');
     window.location.href = "index.html";
 }
