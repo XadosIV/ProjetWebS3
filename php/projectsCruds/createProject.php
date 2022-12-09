0490449083a51923effbbd404e6b92c977548fb7
TomyElPapel <101008603+TomyElPapel@users.noreply.github.com>
2022-12-09 15:35:07 +0100

    C BO

---
+++ php/projectsCruds/createProject.php
@@ -0,0 +1,17 @@
+<?php
+
+include("../connectSQL.php");
+
+$request_body = file_get_contents('php://input');
+$data = json_decode($request_body, true);
+
+
+$ownerId = $data["ownerId"];
+
+
+$sql="INSERT INTO `siteproject`(`name`, `ownerId`) VALUES ('newProject',$ownerId)";
+
+mysqli_query($connection , $sql);
+echo $connection -> insert_id;
+include("../disconnectSQL.php");
+?>
